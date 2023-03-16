33b9c40ef408fa53fd3eff532b96ba3d9545665a
Jay <j4kub07@protonmail.com>
2023-03-16 20:28:26 +0100

    NoteList UI change and beginning of dark mode

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { SaveNotesList } from "../components/SavedNotesList";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, useColorScheme } from "react-native";
 
 export const HomeScreen: React.FC = () => {
+  const theme = useColorScheme();
   return (
-    <View style={styles.container}>
+    <View style={theme == 'dark' ? styles.container_dark : styles.container_light}>
       <SaveNotesList />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container_light: {
     backgroundColor: "#fff",
     flex: 1,
   },
+  container_dark: {
+    backgroundColor: "#0a0b0a",
+    flex: 1,
+  }
 });
